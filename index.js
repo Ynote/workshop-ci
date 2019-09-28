6c3730f6e3d6b0efc3dba1c067206b5ec9fe3add
Fanny Cheung <fanny@ynote.hk>
2019-09-28 15:31:38 +0200

    [CodeExample] Add Express simple page

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, function() {
+  console.log(`Listening on ${ PORT }…`)
+})
